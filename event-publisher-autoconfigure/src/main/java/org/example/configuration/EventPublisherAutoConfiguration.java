6ac657be0d9ba06cac3101246047294d41e30e0f
Nelson Germán Ghezzi <nelsonghezzi@users.noreply.github.com>
2020-08-16 18:50:01 -0300

    Conditionally registered EventPublisher bean

--- event-publisher-autoconfigure/src/main/java/org/example/configuration/EventPublisherAutoConfiguration.java
+++ event-publisher-autoconfigure/src/main/java/org/example/configuration/EventPublisherAutoConfiguration.java
@@ -4,6 +4,7 @@ import org.example.event.DummyEventPublisher;
 import org.example.event.EventListener;
 import org.example.event.EventPublisher;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -22,6 +23,7 @@ public class EventPublisherAutoConfiguration {
     EventPublisherProperties properties;
 
     @Bean
+    @ConditionalOnMissingBean
     EventPublisher eventPublisher(List<EventListener> listeners) {
         return new DummyEventPublisher(listeners, properties.isLogTime());
     }
